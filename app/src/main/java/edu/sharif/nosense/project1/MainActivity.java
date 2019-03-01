8669bdda840eb38d4fb56fe8fc7f72dd3c0361e9
HamedKh99 <goo.hamedacc@gmail.com>
2019-03-01 17:53:46 +0330

    created layout of app

--- app/src/main/java/edu/sharif/nosense/project1/MainActivity.java
+++ app/src/main/java/edu/sharif/nosense/project1/MainActivity.java
@@ -1,7 +1,7 @@
 package edu.sharif.nosense.project1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
